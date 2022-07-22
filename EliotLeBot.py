a618b492579a0d3d993c588f6422082438eefc66
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-22 22:35:42 +0200

    Fix syntax

--- EliotLeBot.py
+++ EliotLeBot.py
@@ -1,95 +1,91 @@
 import time
 import discord
-import os ,sys
+import os
+import sys
 import json
 from discord.ext import commands
 
-def get_prefix(client, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+
+def get_prefix(message):
+    with open('prefixes.json', 'r') as file:
+        prefixes = json.load(file)
 
     return prefixes[str(message.guild.id)]
 
 
-client = commands.Bot(intents= discord.Intents.default() ,command_prefix = get_prefix)
-
+client = commands.Bot(intents = discord.Intents.default(), command_prefix = get_prefix)
 
 
 @client.event
 async def on_guild_join(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+    with open('prefixes.json', 'r') as file:
+        prefixes = json.load(file)
 
     prefixes[str(guild.id)] = '.'
 
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent = 4 )
-
+    with open('prefixes.json', 'w') as file:
+        json.dump(prefixes, file, indent = 4)
 
 
 @client.event 
 async def on_guild_remove(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+    with open('prefixes.json', 'r') as file:
+        prefixes = json.load(file)
 
     prefixes.pop(str(guild.id))
 
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent = 4 )
-
+    with open('prefixes.json', 'w') as file:
+        json.dump(prefixes, file, indent = 4)
 
 
 @client.command()
 async def changeprefix(ctx, prefix):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
+    with open('prefixes.json', 'r') as file:
+        prefixes = json.load(file)
 
     prefixes[str(ctx.guild.id)] = prefix
 
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent = 4 )
+    with open('prefixes.json', 'w') as file:
+        json.dump(prefixes, file, indent = 4)
     await ctx.send(f'Le préfixe de commande est désomais " {prefix} "')
-
 
 
 def restart_bot():
     os.execv(sys.executable, ['python'] + sys.argv)
 
 
-
 @client.command(aliases=['reboot'])
 @commands.has_role("Devs")
-async def restart(ctx):
+async def restart():
     channel = client.get_channel(int(994322769632309350))   
-    embed=discord.Embed(title=" 🦌  EliotLeBot Reboot  ♻️ ", description=" Eliot redémare ... Patientez 2s", color=0xEFB422)
+    embed = discord.Embed(title=" 🦌  EliotLeBot Reboot  ♻️ ", description=" Eliot redémare ... Patientez 2s", color=0xEFB422)
     await channel.purge(limit=10)
     await channel.send(embed=embed)
     time.sleep(2)
     restart_bot()
-    
 
 
-@client.command(aliases=['Load', 'laod', 'Laod'])
+@client.command(aliases=['Load'])
 @commands.has_role("Devs")
 async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+    await client.load_extension(f'cogs.{extension}')
     print('Loaded')
+    await ctx.send(f'{extension} has been Loaded ')
 
 
-
-@client.command(aliases=['Unload', 'unlaod', 'Unlaod'])
+@client.command(aliases=['Unload'])
 @commands.has_role("Devs")
 async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
+    await client.unload_extension(f'cogs.{extension}')
     print('Unloaded')
+    await ctx.send(f'{extension} has been Unloaded')
 
 
-
-@client.command(aliases=['Reload', 'relaod', 'Relaod'])
+@client.command(aliases=['Reload'])
 @commands.has_role("Devs")
 async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
+    await client.unload_extension(f'cogs.{extension}')
+    await client.load_extension(f'cogs.{extension}')
     print('Reloaded')
     await ctx.send(f'{extension} has been reloaded')
 
@@ -100,7 +96,7 @@ for filename in os.listdir('./cogs'):
 
 
 with open("Token.0", "r", encoding="utf-8") as f:
-    bottoken =f.read()
+    bottoken = f.read()
 
 
 client.run(bottoken)
