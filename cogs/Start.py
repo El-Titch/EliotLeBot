76237147223ef42ec128c7763dbefbc921f25186
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-24 01:36:48 +0200

    Fix syntax + suppression Logout (ne fonctionnait pas )

--- cogs/Start.py
+++ cogs/Start.py
@@ -1,33 +1,21 @@
-from logging import shutdown
 import discord
+from .RolesView import Roleview
 from discord.ext import commands
+from discord import app_commands
 
 
-class Start(commands.Cog):
+class Start(discord.Client, commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
 
-    @commands.Cog.listener()
     async def on_ready(self):
-        print("Connected")
+        print(f"Connected as {self.user}")
         channel = self.client.get_channel(int(994322769632309350)) #channel propre au démarrage du bot    
         embed=discord.Embed(title=" 🦌  EliotLeBot est connecté  ⭕ ", description="Le Eliot s'est connecté", color=0x51F56A)
         await channel.purge(limit=10)
         await channel.send(embed=embed)
-
-
-
-    @commands.command()
-    async def logout(self, ctx):
-        channel = self.client.get_channel(int(994322769632309350))
-        embed=discord.Embed(title=" 🦌  EliotLebot est déconnecté ❌ ", description="Le Eliot s'est déconnecté", color=0xff3737)
-        await ctx.channel.purge(limit=10)
-        await ctx.channel.send(embed=embed)
-        await ctx.shutdown
-
-
 
 
 async def setup(client):
