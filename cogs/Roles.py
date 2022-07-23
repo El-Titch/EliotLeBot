6be0b924c8451fd71c46d35ffed9e63695357fc1
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-24 01:34:51 +0200

    fix syntax

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -1,25 +1,24 @@
 from .RolesView import Roleview
 from discord.ext import commands
 
+
 class roles(commands.Cog):
-    """Créer des boutons pour attribuer des roles"""
-
-    def __init__ (self, client : commands.Bot):
-        self.client = client
 
 
+	def __init__(self,client):
+		self.client = client
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.client.add_view(Roleview())
+	@commands.Cog.listener()
+	async def on_ready(self, client):
+		self.client.add_view(Roleview(client))
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def roles(self, ctx: commands.Context):
-        """Creates a new role view"""
-        await ctx.send("cliquez",view=Roleview())
+	@commands.command()
+	@commands.has_permissions(administrator=True)
+	async def roles(self, client):
+		"""Créer des boutons pour attribuer des roles"""
+		await client.send("cliquez", view=Roleview(client))
 
 
 
 async def setup(client: commands.Bot):
-    await client.add_cog(roles(client))
+	await client.add_cog(roles(client))
