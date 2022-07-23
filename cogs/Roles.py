fafdf645916595e5353eb1987b8943c03487fccc
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-23 03:48:45 +0200

    Refont du Cog Roles.py

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -1,26 +1,25 @@
-import discord
-from discord.utils import get
+from .RolesView import Roleview
 from discord.ext import commands
 
 class roles(commands.Cog):
+    """Créer des boutons pour attribuer des roles"""
 
-
-    def __init__ (self, client):
+    def __init__ (self, client : commands.Bot):
         self.client = client
 
 
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, member: discord.Member):
-        channel = self.client.get_channel(998373291091841124)   #Id du channel Règles
-        if reaction.message.channel.id != channel :
-            return
-        if member.reaction.emoji == "☑️":
-            role = get(self.guild.roles, id=998378262856216638) #Id du role Follower
-            await member.add_roles(member, role)
+    async def on_ready(self):
+        self.client.add_view(Roleview())
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def roles(self, ctx: commands.Context):
+        """Creates a new role view"""
+        await ctx.send("cliquez",view=Roleview())
 
 
 
-
-async def setup(client):
+async def setup(client: commands.Bot):
     await client.add_cog(roles(client))
