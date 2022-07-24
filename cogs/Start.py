524bc083afba7da584483915406b1b9fb110005d
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-24 15:10:24 +0200

    Fix import

--- cogs/Start.py
+++ cogs/Start.py
@@ -1,17 +1,15 @@
 import discord
-from .RolesView import Roleview
 from discord.ext import commands
-from discord import app_commands
 
 
-class Start(discord.Client, commands.Cog):
+class Start(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
 
     async def on_ready(self):
-        print(f"Connected as {self.user}")
+        print(f"Connected")
         channel = self.client.get_channel(int(994322769632309350)) #channel propre au démarrage du bot    
         embed=discord.Embed(title=" 🦌  EliotLeBot est connecté  ⭕ ", description="Le Eliot s'est connecté", color=0x51F56A)
         await channel.purge(limit=10)
