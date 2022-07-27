795851eab815211b08a02581aa2fb535edab5785
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-27 17:43:59 +0200

    Ajout Help commentaire

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -23,31 +23,34 @@ class Moderation(commands.Cog):
 
     @commands.command(aliases = ['Kick'])
     @commands.has_permissions(kick_members = True)
-    async def kick(self, ctx, member: commands.MemberConverter, *, reason = None):
+    async def kick(self, ctx, member: discord.Member, *, reason = None):
+        """Permet d'exclure un member du discord"""
         await ctx.guild.kick(member, reason = reason)
         await ctx.send(f"""L'utilisateur {member} a été kick pour " {reason} " """)
 
     @commands.command(aliases = ['tban'])  # Bannissement temporaire
     @commands.has_permissions(ban_members = True)
     async def Tban(self, ctx, member: commands.MemberConverter, duration: DurationConverter):
-
+        """Permet de bannir temporairement un membre du discord"""
         multiplier = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}
         amount, unit = duration
 
         await ctx.guild.ban(member)
-        await ctx.send(f'{member} has been banned for {amount}{unit}.')
+        await ctx.send(f'{member} a été banni pour une durée de {amount}{unit}.')
         await asyncio.sleep(amount * multiplier[unit])
         await ctx.guild.unban(member)
 
     @commands.command(aliases = ['Ban'])  # Bannissement permanent
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member: commands.MemberConverter, *, reason = None):
+        """Permet de bannir de manière permanente un membre du discord"""
         await ctx.guild.ban(member, reason = reason)
         await ctx.send(f"""L'utilisateur {member} a été banni pour " {reason} " """)
 
     @commands.command(aliases = ['Unban'])
     @commands.has_permissions(ban_members = True)
-    async def unban(self, ctx, member: commands.MemberConverter, *, reson = None):
+    async def unban(self, ctx, member: commands.MemberConverter):
+        """Permet de débannir un member du discord"""
         user = await commands.UserConverter().convert(ctx, str(member))
         if user is None:
             id_ = await commands.IDConverter().convert(ctx, str(member))
@@ -65,7 +68,7 @@ class Moderation(commands.Cog):
     @commands.command(aliases = ['Mute'])
     @commands.has_permissions()
     async def mute(self, member: discord.Member, *, duration: DurationConverter):
-
+        """Permet de temporairement rendre muet un membre du discord  """
         role = get(member.guild.roles, name = "Muted")
         multiplier = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}
         amount, unit = duration
