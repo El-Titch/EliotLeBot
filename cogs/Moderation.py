afab6f412e8e4dff826d436cbb341e0512d6a75f
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-27 12:47:05 +0200

    fix update to 2.0

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,7 +1,19 @@
-import discord 
+import discord
 import asyncio
 from discord.utils import get
-from discord.ext import commands 
+from discord.ext import commands
+
+
+class DurationConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        amount = argument[:-1]
+        unit = argument[-1]
+
+        if amount.isdigit() and unit in ['s', 'm', 'h', 'd']:
+            return (int(amount), unit)
+
+        raise commands.BadArgument(message = 'Not valid duration')
+
 
 class Moderation(commands.Cog):
 
@@ -9,31 +21,17 @@ class Moderation(commands.Cog):
         self.client = client
 
 
-    class DurationConverter(commands.Converter):
-            async def convert(self, ctx, argument):
-                amount = argument[:-1]
-                unit = argument[-1]
-
-                if amount.isdigit() and unit in ['s', 'm', 'h', 'd']:
-                    return (int(amount), unit)
-
-                raise commands.BadArgument(message='Not valid duration')
-
-
-
-    @commands.command(aliases=['Kick'])
-    @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, member: commands.MemberConverter, *, reason=None):
-        await ctx.guild.kick(member, reason=reason)
+    @commands.command(aliases = ['Kick'])
+    @commands.has_permissions(kick_members = True)
+    async def kick(self, ctx, member: commands.MemberConverter, *, reason = None):
+        await ctx.guild.kick(member, reason = reason)
         await ctx.send(f"""L'utilisateur {member} a été kick pour " {reason} " """)
 
+    @commands.command(aliases = ['tban'])  # Bannissement temporaire
+    @commands.has_permissions(ban_members = True)
+    async def Tban(self, ctx, member: commands.MemberConverter, duration: DurationConverter):
 
-    
-    @commands.command(aliases=['tban']) # Bannissement temporaire
-    @commands.has_permissions(ban_members=True)
-    async def Tban(self, ctx, member : commands.MemberConverter, duration : DurationConverter):
-
-        multiplier = {'s' : 1, 'm': 60, 'h' : 3600, 'd' : 86400}
+        multiplier = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}
         amount, unit = duration
 
         await ctx.guild.ban(member)
@@ -41,26 +39,22 @@ class Moderation(commands.Cog):
         await asyncio.sleep(amount * multiplier[unit])
         await ctx.guild.unban(member)
 
-
-
-    @commands.command(aliases=['Ban']) # Bannissement permanent 
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: commands.MemberConverter, *, reason=None):
-        await ctx.guild.ban(member, reason=reason)
+    @commands.command(aliases = ['Ban'])  # Bannissement permanent
+    @commands.has_permissions(ban_members = True)
+    async def ban(self, ctx, member: commands.MemberConverter, *, reason = None):
+        await ctx.guild.ban(member, reason = reason)
         await ctx.send(f"""L'utilisateur {member} a été banni pour " {reason} " """)
 
-
-
-    @commands.command(aliases=['Unban'])
-    @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, *, member: commands.MemberConverter):
-        user = await commands.UserConverter().convert(ctx, member)
+    @commands.command(aliases = ['Unban'])
+    @commands.has_permissions(ban_members = True)
+    async def unban(self, ctx, member: commands.MemberConverter, *, reson = None):
+        user = await commands.UserConverter().convert(ctx, str(member))
         if user is None:
-            id_ = await commands.IDConverter().convert(str(member))
+            id_ = await commands.IDConverter().convert(ctx, str(member))
             if id_ is not None:
                 try:
                     user = await self.client.fetch_user(int(id_.group(1)))
-                except discord.Notfound:
+                except discord.NotFound:
                     user = None
             if user is None:
                 await ctx.send('User not found')
@@ -68,25 +62,22 @@ class Moderation(commands.Cog):
         await ctx.guild.unban(user)
         await ctx.send(f'Unbanned {user}')
 
-    
-    @commands.command(aliases=['Mute'])
+    @commands.command(aliases = ['Mute'])
     @commands.has_permissions()
-    async def mute(self, ctx, member: commands.MemberConverter, *, duration: DurationConverter):
+    async def mute(self, member: discord.Member, *, duration: DurationConverter):
 
-        role = get(member.guild.roles, name="Muted")
-        multiplier = {'s' : 1, 'm': 60, 'h' : 3600, 'd' : 86400}
+        role = get(member.guild.roles, name = "Muted")
+        multiplier = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}
         amount, unit = duration
-
 
         if member.dm_channel == None:
             await member.create_dm()
-        await member.dm_channel.send(content=f"Vous avez été muté pour une durée de {amount}{unit} \n Il vous sera impossible de converser sur le serveur jusqu'à la fin du temps imparti.")
+        await member.dm_channel.send(
+			content = f"Vous avez été muté pour une durée de {amount}{unit} \n Il vous sera impossible de converser sur le serveur jusqu'à la fin du temps imparti.")
         await member.add_roles(role)
         await asyncio.sleep(amount * multiplier[unit])
         await member.remove_roles(role)
 
-        
-        
 
 async def setup(client):
     await client.add_cog(Moderation(client))
