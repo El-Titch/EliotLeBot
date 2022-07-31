60a4fbbcb0174199c4e80f2b523ea60b20f1957a
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-07-31 22:58:32 +0200

    Fix styntax

--- cogs/PermissionsChecker.py
+++ cogs/PermissionsChecker.py
@@ -8,7 +8,6 @@ class PermissionsChecker(commands.Cog):
     @commands.command(name='perms', aliases=['perms_for', 'permissions'])
     @commands.guild_only()
     async def check_permissions(self, ctx, *, member: discord.Member=None):
-
         if not member:
             member = ctx.author
 
@@ -17,7 +16,7 @@ class PermissionsChecker(commands.Cog):
 
         # And to make it look nice, we wrap it in an Embed.
         embed = discord.Embed(title='Permissions for:', description=ctx.guild.name, colour=member.colour)
-        embed.set_author(icon_url=member.avatar_url, name=str(member))
+        embed.set_author(name = member, icon_url = member.avatar)
 
         # \uFEFF is a Zero-Width Space, which basically allows us to have an empty field name.
         embed.add_field(name='\uFEFF', value=perms)
