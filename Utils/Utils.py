990ec791c153fe0a80228a2abc3ff3efb4f32754
Eltitch <raphael.alcoba.lopez@gmail.com>
2022-08-04 01:10:40 +0200

    Fix synthax and def function

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -1,18 +1,18 @@
 import config
-from discord.utils import get
+import re
 
 def custom_id(view:str, id: int) -> str:
 	"""Return the View and the ID"""
 	return f"{config.CLIENT_NAME}:{view}:{id}"
 
-async def create_voice_channel(guild, channel_name, category_name = "RAPH", user_limit = 6):
+async def create_voice_channel(guild, channel_name, category_name = "Games", user_limit = 6):
 
 	category = get_category_by_name(guild, category_name)
 	await guild.create_voice_channel(channel_name, category = category, user_limit= user_limit)
 	channel = get_channel_by_name(guild, channel_name)
 	return channel
 
-async def create_text_channel(guild, channel_name, category = "RAPH", user_limit = 6):
+async def create_text_channel(guild, channel_name, category = "Games", user_limit = 6):
 
 	category = guild.get_category_by_name(guild, category)
 	await guild.create_text_channel(channel_name, category = category)
@@ -35,3 +35,6 @@ def get_channel_by_name(guild, channel_name):
 			channel = c
 			break
 	return channel
+
+def msg_contains_word(msg, word):
+	return re.search(fr"\b({word})\b", msg) is not None
